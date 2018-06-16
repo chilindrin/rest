df2d3e96fd526e0b94bd6b394eae0b70c9159410
Guerrero <gua@isoad.isogmbh.de>
2018-06-16 16:47:43 +0200

    New Interface for ReadingList

---
+++ readinglist/src/main/java/readinglist/ReadingListRepository.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package readinglist;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author gua
+ */
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+	
+	List<Book> findByReader(String reader);
+
+}
