99fae2352238045510e0bd5359158393c28141a4
Guerrero <gua@isoad.isogmbh.de>
2020-02-17 21:31:55 +0100

    Ignoriere noch paar Dateien

---
+++ data-migration/src/main/java/com/chillin/co/JobStarter.java
@@ -0,0 +1,11 @@
+package com.chillin.co;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class JobStarter implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+            
+    }
+}
